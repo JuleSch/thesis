7ba2bc1228116fb83f3ed87df576bd4594e7dc58
Julia Schöwe <schoewe@dresearch-fe.de>
2018-02-05 16:18:35 +0100

    Init

---
+++ newBach/src/app/test/test.component.ts
@@ -0,0 +1,47 @@
+import { Component , OnInit} from '@angular/core';
+import {JsonService} from '../json.service';
+
+
+@Component({
+  selector: 'app-test',
+  template: `<table>
+    <tr>
+      <th>Id</th>
+      <th>Vorname</th>
+      <th>Name</th>
+    </tr>
+    <tr *ngFor="let data of myJson">
+      <td>{{data.id | json}}</td>
+      <td>{{data.firstName | json}}</td>
+      <td>{{data.lastName | json}}</td>
+    </tr>
+  </table>
+
+  `
+})
+
+
+export class TestComponent implements OnInit{
+  myJson;
+  myData;
+
+  constructor(private jsonService: JsonService) {}
+
+  ngOnInit(){
+    this.showJson();
+  }
+
+
+  showJson(){
+    this.jsonService.getJson()
+      .subscribe(data =>  this.myJson = data);
+  }
+}
+
+
+
+
+
+
+
+
