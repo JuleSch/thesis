b425a59f929651dbe7af93528416ce14c8df10bc
Julia Schöwe <schoewe@dresearch-fe.de>
2018-02-06 14:23:06 +0100

    Selectbox mit Daten aus einer Jsondatei implementiert.

---
+++ src/app/select/select.component.ts
@@ -0,0 +1,48 @@
+import {Component, OnInit, Input, ViewChild} from '@angular/core';
+import {ViewContainerRef, ComponentFactory, ComponentFactoryResolver} from '@angular/core';
+import {JsonService} from '../json.service';
+
+@Component({
+  selector: 'selectbox',
+  template: `<div>
+    <label>{{selectLabel}}:
+      <select>
+        <option *ngFor="let v of values" [value]="v">{{v.id}}</option> <!-- ngValue unterstützt im Vergleich zu value alle Datentypen, value nur strings-->
+      </select>
+    </label>
+  </div>`
+})
+
+export class Selectbox {
+  @Input() selectLabel = '';
+  @Input() values: string[];
+}
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css']
+})
+export class SelectComponent implements OnInit {
+
+  @ViewChild('container', {read: ViewContainerRef}) container; // @ViewChild --> Verbindung zum selector; ViewcontainerRef, denifiert es als ViewContainer
+  selectboxFactory: ComponentFactory<Selectbox>;
+
+  constructor(private resolver: ComponentFactoryResolver, private jsonService: JsonService) {
+  this.selectboxFactory = this.resolver.resolveComponentFactory(Selectbox);
+}
+
+addSelectbox(selectLabel: string, data: any) {
+  const selectboxRef = this.container.createComponent(this.selectboxFactory); // mit createComponent erzeuge ist das Element
+  selectboxRef.instance.selectLabel = selectLabel;
+  selectboxRef.instance.values = data;
+
+}
+
+ngOnInit() {
+
+  this.jsonService.getJson().subscribe(data => this.addSelectbox('Hier die Box', data));
+
+}
+
+}
