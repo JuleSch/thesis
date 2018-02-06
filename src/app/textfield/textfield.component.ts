50dcd102406f70d49d6b1c5b06ea42b9aab91e46
Julia Schoewe <s0543954@htw-berlin.de>
2018-02-06 22:01:20 +0100

    readonly in Textfeld optional auswaehlbar gemacht.

--- src/app/textfield/textfield.component.ts
+++ src/app/textfield/textfield.component.ts
@@ -1,13 +1,14 @@
 import {Component, OnInit, Input, ViewChild} from '@angular/core';
 import {ViewContainerRef, ComponentFactory, ComponentFactoryResolver} from '@angular/core';
 
-
+//TODO: herausfinden warum Syntax mit "!" und nicht mit {{}}
 @Component({
   selector: 'textfield',
-  template: `<div><label>{{label}}<input></label></div>`
+  template: `<div><label>{{label}}<input [readonly]="!readonly"></label></div>`
 })
 export class Textfield {
   @Input() label = 'Label';
+  @Input() readonly: boolean;
 }
 
 
@@ -26,13 +27,15 @@ export class TextfieldComponent implements OnInit {
   }
 
 
-  addTextfield(label: string){
+  addTextfield(label: string, readonly: boolean){
     const textfieldRef = this.container.createComponent(this.textfieldFactory); // mit createComponent erzeuge ist das Element
     textfieldRef.instance.label = label;
+    textfieldRef.instance.readonly = readonly;
+console.log(readonly);
   }
 
   ngOnInit(){
-    this.addTextfield('neues Label');
+    this.addTextfield('neues Label', true);
     // this.container.remove(this.container.length - 1); --> löscht das letzte Textfeld
     // mit der Mtehode "detach" kann man die Komponente erstmal aus dem DOM entfernen, aber zu einem späteren zeitpunkt mit der insert-Methode wieder einfügen.
 
